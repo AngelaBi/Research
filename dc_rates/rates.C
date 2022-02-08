e9855d8689344864c6161ea8ae277591d4983c49
AngelaBi <abiselli@fairfield.edu>
2022-02-08 12:18:03 -0500

    cleaning up

--- dc_rates/rates.C
+++ dc_rates/rates.C
@@ -65,8 +65,9 @@
 	const int NREG     = 3;
 	string sreg[NREG] = {"reg1", "reg2", "reg3"};
 	int REG = 0;
-	
-	double factordatasimu[NREG] = {500./250.,1400./250.,1200./500   };  // additional factor to scale the different configurations
+  //Factor to scale the occupancies according to the time windows	
+	//double factordatasimu[NREG] = {500./250.,1400./250.,1200./500   };  // additional factor to scale the different configurations
+	double factordatasimu[NREG] = {1,1,1};  // additional factor to scale the different configurations
 	// near target
 	zlimits[0][0][0] = -10;    zlimits[0][1][0] =  150;
 	zlimits[2][0][0] = -100;   zlimits[2][1][0] = 1000;
